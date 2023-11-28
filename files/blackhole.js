564772af747fe778eef7b619f648c2612b9f4d5b
KelvinCreighton <kelvincreighton413@gmail.com>
2023-11-28 11:43:12 -0700

    Added accretion disk :)

--- files/blackhole.js
+++ files/blackhole.js
@@ -4,6 +4,26 @@ class Blackhole {
         this.y    = y;
         this.mass = mass;
         this.size = 50;
+        
+        // Accretion Disk (AD)
+        this.ADSize = 0;
+        this.ADMaxSize = 10;
+        this.jetActivate = false;
+        this.jetMassFlow = 0.05;
+    }
+
+    jetUpdate() {
+        // Activate the jet when the accretion disk gets too large
+        if (this.ADSize >= this.ADMaxSize)
+            this.jetActivate = true;
+        // Decrease the accretion disk when the jet is activated
+        if (this.jetActivate && this.ADSize > 0)
+            this.ADSize -= this.jetMassFlow;
+        // Deactivate the jet when the accretion disk reaches 0 again
+        if (this.jetActivate && this.ADSize <= 0) {
+            this.jetActivate = false;
+            this.ADSize = 0;
+        }
     }
 
     // Calculate gravitational force between this black hole and another satellite
@@ -11,10 +31,40 @@ class Blackhole {
         return (G * this.mass * objectMass) / (distance*distance);
     }
 
+    growAD(mass) {
+        let growPercent = 0.01
+        this.ADSize += mass * growPercent;
+    }
+
     display() {
-        ellipse(this.x, this.y, this.size*2, this.size*2);
-        strokeWeight(3);
-        stroke("#ffffff");
+        // Simple repeating ellipse as the accretion disk grows
+        let ringGrowSize = 4
+        strokeWeight(ringGrowSize/2.0);
+        for (let i = 0; i < this.ADSize; i++) {
+            ellipse(this.x, this.y, this.size*2 + i*ringGrowSize, this.size*2 + i*ringGrowSize);
+            stroke("#aaaaff");
+        }
+
+        strokeWeight(4);
+        // Display Jet
+        if (this.jetActivate) {
+            // Up
+            for (let i = 0; i < floor(random(4,8)); i++) {
+                line(this.x, this.y, this.x + random(-this.size, this.size), this.y + random(this.size*2, this.size*6));
+                stroke("#ffaaaa");
+            }
+            // Down
+            for (let i = 0; i < floor(random(4,8)); i++) {
+                line(this.x, this.y, this.x + random(-this.size, this.size), this.y - random(this.size*2, this.size*6));
+                stroke("#ffaaaa");
+            }
+        }
+        
+        
+        // Display Blackhole
+        // ellipse(this.x, this.y, this.size*2, this.size*2);
+        // strokeWeight(3);
+        // stroke("#ffffff");        
     }
 
     printInfo() {
--- files/canvas_lib.js
+++ files/canvas_lib.js
@@ -94,3 +94,21 @@ function ellipse(x, y, rX, rY, rotation=0, start=0, end=(Math.PI*2)) {
     Canvas.beginPath();
     Canvas.ellipse(x, y, rX, rY, rotation, start, end);
 }
+
+
+/*
+-------------------------------
+  Additional Helpful Functions
+-------------------------------
+*/
+function random(a, b) {
+	return (Math.random() * ((b-a)+1)) + a;
+}
+
+function floor(n) {
+	return Math.floor(n);
+}
+
+function ceil(n) {
+	return Math.ceil(n);
+}
